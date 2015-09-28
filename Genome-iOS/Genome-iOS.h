48880501c1bc50e5f79f70772afce05ddb95c5aa
Pepi <pepibumur@gmail.com>
2015-09-28 22:28:36 +0200

    Added Carthage support

---
+++ Genome-iOS/Genome-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Genome-iOS.h
+//  Genome-iOS
+//
+//  Created by Pedro Piñera Buendía on 28/09/15.
+//  Copyright © 2015 lowriDevs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Genome-iOS.
+FOUNDATION_EXPORT double Genome_iOSVersionNumber;
+
+//! Project version string for Genome-iOS.
+FOUNDATION_EXPORT const unsigned char Genome_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Genome_iOS/PublicHeader.h>
+
+
