d28ae9f7fec0f37c7a24ace2cf25cbc402a619ec
Logan Wright <logan.william.wright@gmail.com>
2015-10-18 18:49:01 -0400

    resolving issue of nsnull in non-foundation environment

--- GenomeTests/GenomeTests.swift
+++ GenomeTests/GenomeTests.swift
@@ -11,6 +11,17 @@ import XCTest
 
 class GenomeTests: XCTestCase {
     
+    let json: JSON = [
+        "name" : NSNull()
+    ]
+    
+    struct Model : BasicMappable {
+        var name: String? = ""
+        mutating func sequence(map: Map) throws {
+            try name <~ map["name"]
+        }
+    }
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,6 +33,8 @@ class GenomeTests: XCTestCase {
     }
     
     func testExample() {
+        let model = try! Model.mappedInstance(json)
+        XCTAssert(model.name == nil)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
