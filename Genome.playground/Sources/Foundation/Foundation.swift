121beb09ddab6f5f5fa2e96644ee3e6a93f55a17
Logan Wright <logan.william.wright@gmail.com>
2016-01-06 19:38:05 -0500

    removing unnecessary initializers

--- Genome.playground/Sources/Foundation/Foundation.swift
+++ Genome.playground/Sources/Foundation/Foundation.swift
@@ -6,18 +6,6 @@
 //  Copyright © 2015 lowriDevs. All rights reserved.
 //
 
-extension BasicMappable {
-    public init(js: AnyObject, context: [String : AnyObject] = [:]) throws {
-        let safeJson = Json.from(js)
-        let safeContext = Json.from(context)
-        try self.init(js: safeJson, context: safeContext)
-    }
-    
-    public init(js: [String : AnyObject], context: [String : AnyObject] = [:]) throws {
-        try self.init(js: js as AnyObject, context: context)
-    }
-}
-
 extension MappableObject {
     public init(js: AnyObject, context: [String : AnyObject] = [:]) throws {
         let safeJson = Json.from(js)
