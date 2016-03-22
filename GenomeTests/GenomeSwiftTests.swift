479764842acc6b785fa92856f19a43301250fa02
Logan Wright <logan.william.wright@gmail.com>
2016-03-21 23:35:14 -0400

    fixing inout

--- GenomeTests/GenomeSwiftTests.swift
+++ GenomeTests/GenomeSwiftTests.swift
@@ -131,7 +131,11 @@ class GenomeSideLoadTests: XCTestCase {
         XCTAssert(allFoods.count == 4)
 
         var peeps: [Person] = try! [Person](node: nodeArrayOfPeople, context: SideLoadTestNode)
-        peeps = peeps.map { (var person) -> Person in person.associateFavoriteFoods(allFoods); return person }
+        peeps = peeps.map { (person) -> Person in
+            var mutable = person
+            mutable.associateFavoriteFoods(allFoods);
+            return mutable
+        }
         XCTAssert(peeps.count == 2)
         
         let a = peeps.first!
