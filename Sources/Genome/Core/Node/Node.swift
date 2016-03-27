f3713e5591cc4bd31042433da97eaae4cac61d78
Logan Wright <logan.william.wright@gmail.com>
2016-03-27 00:59:48 -0400

    adding/starting fuzzy mapping capabilities

--- Sources/Genome/Core/Node/Node.swift
+++ Sources/Genome/Core/Node/Node.swift
@@ -6,6 +6,21 @@
 //  Copyright (c) 2014 Fuji Goro. All rights reserved.
 //
 
+public enum TypeEnforcing {
+    case strict
+    case fuzzy
+    
+    public var isStrict: Bool {
+        return self == .strict
+    }
+    
+    public var isFuzzy: Bool {
+        return self == .fuzzy
+    }
+}
+
+public var TypeEnforcingLevel: TypeEnforcing = .strict
+
 public enum Node {
     case NullValue
     case BooleanValue(Bool)
@@ -75,11 +90,23 @@ extension Node {
 
 extension Node {
     public var isNull: Bool {
+        // Type enforcing level doesn't really apply here
         guard case .NullValue = self else { return false }
         return true
     }
-    
+}
+
+extension Node {
     public var boolValue: Bool? {
+        switch TypeEnforcingLevel {
+        case .strict:
+            return strictBoolValue
+        case .fuzzy:
+            return fuzzyBoolValue
+        }
+    }
+    
+    public var strictBoolValue: Bool? {
         if case let .BooleanValue(bool) = self {
             return bool
         } else if let integer = intValue where integer == 1 || integer == 0 {
@@ -93,12 +120,47 @@ extension Node {
         }
     }
     
+    public var fuzzyBoolValue: Bool? {
+        if let strict = strictBoolValue {
+            return strict
+        } else if let n = strictNumberValue {
+            return n > 0
+        } else if let s = strictStringValue {
+            return Bool(s)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Node {
     public var numberValue: Double? {
+        switch TypeEnforcingLevel {
+        case .strict:
+            return strictNumberValue
+        case .fuzzy:
+            return fuzzyNumberValue
+        }
+    }
+    
+    public var strictNumberValue: Double? {
         guard case let .NumberValue(number) = self else {
             return nil
         }
         
         return number
+    }
+    
+    public var fuzzyNumberValue: Double? {
+        if let n = strictNumberValue {
+            return n
+        } else if let s = strictStringValue {
+            return Double(s)
+        } else if let b = strictBoolValue {
+            return b ? 1 : 0
+        } else {
+            return nil
+        }
     }
     
     public var floatValue: Float? {
@@ -108,8 +170,19 @@ extension Node {
     public var doubleValue: Double? {
         return numberValue
     }
-    
+}
+
+extension Node {
     public var intValue: Int? {
+        switch TypeEnforcingLevel {
+        case .strict:
+            return strictIntValue
+        case .fuzzy:
+            return fuzzyIntValue
+        }
+    }
+    
+    public var strictIntValue: Int? {
         guard let double = numberValue where double % 1 == 0 else {
             return nil
         }
@@ -117,12 +190,60 @@ extension Node {
         return Int(double)
     }
     
+    public var fuzzyIntValue: Int? {
+        if let i = strictIntValue {
+            return i
+        } else if let s = strictStringValue {
+            return Int(s)
+        } else if let b = strictBoolValue {
+            return b ? 1 : 0
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Node {
     public var uintValue: UInt? {
-        guard let intValue = intValue where intValue >= 0 else { return nil }
+        switch TypeEnforcingLevel {
+        case .strict:
+            return strictUIntValue
+        case .fuzzy:
+            return fuzzyUIntValue
+        }
+    }
+    
+    public var strictUIntValue: UInt? {
+        guard let intValue = strictIntValue where intValue >= 0 else { return nil }
         return UInt(intValue)
     }
     
+    public var fuzzyUIntValue: UInt? {
+        if let ui = strictUIntValue {
+            return ui
+        } else if let i = strictIntValue {
+            return i > 0 ? UInt(i) : 0
+        } else if let s = strictStringValue {
+            return UInt(s)
+        } else if let b = strictBoolValue {
+            return b ? 1 : 0
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Node {
     public var stringValue: String? {
+        switch TypeEnforcingLevel {
+        case .strict:
+            return strictStringValue
+        case .fuzzy:
+            return fuzzyStringValue
+        }
+    }
+    
+    public var strictStringValue: String? {
         guard case let .StringValue(string) = self else {
             return nil
         }
@@ -130,14 +251,71 @@ extension Node {
         return string
     }
     
+    public var fuzzyStringValue: String? {
+        if let s = strictStringValue {
+            return s
+        } else if let i = strictIntValue { // int first so if it's an int, we omit `1.0` in preference of `1`
+            return i.description
+        } else if let n = strictNumberValue {
+            return n.description
+        } else if let b = strictBoolValue {
+            return b.description
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Node {
     public var arrayValue: [Node]? {
+        switch TypeEnforcingLevel {
+        case .strict:
+            return strictArrayValue
+        case .fuzzy:
+            return fuzzyArrayValue
+        }
+    }
+    
+    public var strictArrayValue: [Node]? {
         guard case let .ArrayValue(array) = self else { return nil }
         return array
     }
     
+    public var fuzzyArrayValue: [Node]? {
+        if let a = strictArrayValue {
+            return a
+        } else if let s = strictStringValue {
+            return s.characters
+                .split { $0 == "," }
+                .map(String.init)
+                .map { Node($0) }
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Node {
     public var objectValue: [String : Node]? {
+        switch TypeEnforcingLevel {
+        case .strict:
+            return strictObjectValue
+        case .fuzzy:
+            return fuzzyObjectValue
+        }
+
+    }
+    
+    public var strictObjectValue: [String : Node]? {
         guard case let .ObjectValue(object) = self else { return nil }
         return object
+    }
+    
+    public var fuzzyObjectValue: [String : Node]? {
+        // I'm not sure if there's anything else that should map to an object.
+        // Possibly string w/ `&` or something,
+        // the issue is if there's no `&`, should it return empty dictionary?
+        return strictObjectValue
     }
 }
 
@@ -295,3 +473,123 @@ extension Node: DictionaryLiteralConvertible {
         self = .ObjectValue(object)
     }
 }
+
+// MARK: HELPERS
+
+
+// TODO: Break Out
+extension Bool {
+    /**
+     This function seeks to replicate the expected behavior of `var boolValue: Bool` on `NSString`.  Any variant of `yes`, `y`, `true`, `t`, or any numerical value greater than 0 will be considered `true`
+     */
+    public init(_ string: String) {
+        let cleaned = string
+            .lowercased()
+            .characters
+            .first ?? "n"
+        
+        switch cleaned {
+        case "t", "y", "1":
+            self = true
+        default:
+            if let int = Int(String(cleaned)) where int > 0 {
+                self = true
+            } else {
+                self = false
+            }
+            
+        }
+    }
+}
+
+//extension String {
+//    /**
+//     Parses `key=value` pair data separated by `&`.
+//     
+//     - returns: String dictionary of parsed data
+//     */
+//    internal func keyValuePairs() -> [String: String]? {
+//        var data: [String: String] = [:]
+//        
+//        for pair in self.split("&") {
+//            let tokens = pair.split("=", maxSplits: 1)
+//            
+//            if
+//                let name = tokens.first,
+//                let value = tokens.last,
+//                let parsedName = try? String(percentEncoded: name) {
+//                data[parsedName] = try? String(percentEncoded: value)
+//            }
+//        }
+//        
+//        return data.isEmpty ? nil : data
+//    }
+//}
+//
+//// FROM: Zewo - String
+//
+//extension String {
+//    public func split(separator: Character, maxSplits: Int = .max, omittingEmptySubsequences: Bool = true) -> [String] {
+//        return characters.split(separator: separator, maxSplits: maxSplits, omittingEmptySubsequences: omittingEmptySubsequences).map(String.init)
+//    }
+//    
+//    public init(percentEncoded: String) throws {
+//        struct Error: ErrorProtocol, CustomStringConvertible {
+//            let description: String
+//        }
+//        
+//        let spaceCharacter: UInt8 = 32
+//        let percentCharacter: UInt8 = 37
+//        let plusCharacter: UInt8 = 43
+//        
+//        var encodedBytes: [UInt8] = [] + percentEncoded.utf8
+//        var decodedBytes: [UInt8] = []
+//        var i = 0
+//        
+//        while i < encodedBytes.count {
+//            let currentCharacter = encodedBytes[i]
+//            
+//            switch currentCharacter {
+//            case percentCharacter:
+//                let unicodeA = UnicodeScalar(encodedBytes[i + 1])
+//                let unicodeB = UnicodeScalar(encodedBytes[i + 2])
+//                
+//                let hexString = "\(unicodeA)\(unicodeB)"
+//                
+//                
+//                
+//                guard let character = Int(hexString, radix: 16) else {
+//                    throw Error(description: "Invalid string")
+//                }
+//                
+//                decodedBytes.append(UInt8(character))
+//                i += 3
+//                
+//            case plusCharacter:
+//                decodedBytes.append(spaceCharacter)
+//                i += 1
+//                
+//            default:
+//                decodedBytes.append(currentCharacter)
+//                i += 1
+//            }
+//        }
+//        
+//        var string = ""
+//        var decoder = UTF8()
+//        var iterator = decodedBytes.makeIterator()
+//        var finished = false
+//        
+//        while !finished {
+//            let decodingResult = decoder.decode(&iterator)
+//            switch decodingResult {
+//            case .scalarValue(let char): string.append(char)
+//            case .emptyInput: finished = true
+//            case .error:
+//                throw Error(description: "UTF-8 decoding failed")
+//            }
+//        }
+//        
+//        self.init(string)
+//    }
+//}
