d316a1e74896e705ab297423e27d4560503178cd
Logan Wright <logan.william.wright@gmail.com>
2016-04-27 19:44:36 -0400

    removing unnecessary backing data foundation import

--- Sources/Genome/Core/BackingDataType/BackingDataType.swift
+++ Sources/Genome/Core/BackingDataType/BackingDataType.swift
@@ -61,4 +61,23 @@ extension MappableObject {
         }
         try self.init(with: node, in: context)
     }
+
+    /**
+     Initialize with a backing data dictionary,
+     and Foundation context
+
+     - parameter node:    backing data dictionary
+     - parameter context: context
+
+     - throws: if fails to initialize
+     */
+    public init<T: BackingData>(with node: [String : T], in context: Context = EmptyNode) throws {
+        var mapped: [String : Node] = [:]
+        try node.forEach { key, value in
+            mapped[key] = try value.toNode()
+        }
+
+        let node = Node(mapped)
+        try self.init(with: node, in: context)
+    }
 }
