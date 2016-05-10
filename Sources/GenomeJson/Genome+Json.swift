1446164121c32d6b4ee77c8e1f3e3bc27872a5d6
Brandon McQuilkin <bmcquilkin@cardinalsolutions.com>
2016-05-10 15:09:27 -0400

    Fixed build issues.

--- Sources/GenomeJson/Genome+Json.swift
+++ Sources/GenomeJson/Genome+Json.swift
@@ -14,22 +14,22 @@
 extension Json: BackingData {
     public func toNode() -> Node {
         switch self {
-        case let .string(str):
+        case let .StringValue(str):
             return .string(str)
-        case let .number(num):
+        case let .NumberValue(num):
             return .number(num)
-        case let .bool(bool):
+        case let .BooleanValue(bool):
             return .bool(bool)
-        case let .array(arr):
+        case let .ArrayValue(arr):
             let mapped = arr.map { $0.toNode() }
             return .array(mapped)
-        case let .object(obj):
+        case let .ObjectValue(obj):
             var mutable: [String : Node] = [:]
             obj.forEach { key, val in
                 mutable[key] = val.toNode()
             }
             return .object(mutable)
-        case .null:
+        case .NullValue:
             return .null
         }
     }
@@ -37,22 +37,22 @@ extension Json: BackingData {
     public init(with node: Node, in context: Context) {
         switch node {
         case let .string(str):
-            self = .string(str)
+            self = .StringValue(str)
         case let .number(num):
-            self = .number(num)
+            self = .NumberValue(num)
         case let .bool(bool):
-            self = .bool(bool)
+            self = .BooleanValue(bool)
         case let .array(arr):
             let mapped = arr.map { Json(with: $0, in: context) }
-            self = .array(mapped)
+            self = .ArrayValue(mapped)
         case let .object(obj):
             var mutable: [String : Json] = [:]
             obj.forEach { key, val in
                 mutable[key] = Json(with: val, in: context)
             }
-            self = .object(mutable)
+            self = .ObjectValue(mutable)
         case .null:
-            self = .null
+            self = .NullValue
         }
     }
 }
@@ -92,7 +92,7 @@ extension Dictionary where Key: CustomStringConvertible, Value: NodeConvertible 
         try self.forEach { key, value in
             mutable["\(key)"] = try value.toJson()
         }
-        return .object(mutable)
+        return .ObjectValue(mutable)
     }
 }
 
