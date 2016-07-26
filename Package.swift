53914ea4243e361be873eeddc69ec9792d3cde53
Logan Wright <logan.william.wright@gmail.com>
2016-07-26 15:19:26 -0400

    update packs

--- Package.swift
+++ Package.swift
@@ -2,13 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "Genome",
-    dependencies: [
-        // using qutheory until update core library
-        .Package(url: "https://github.com/qutheory/pure-json", majorVersion: 2, minor: 1)
-    ],
-    exclude: [
-        "Sources/Genome/CoreData"
-    ],
     targets: [
         Target(
             name: "Genome",
@@ -35,5 +28,11 @@ let package = Package(
                  .Target(name: "Genome")
               ]
           ),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/qutheory/json", majorVersion: 0, minor: 2)
+    ],
+    exclude: [
+        "Sources/Genome/CoreData"
     ]
 )
