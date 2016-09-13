73120ab7f11eb391f426b87422f7026facbeae22
Logan Wright <logan.william.wright@gmail.com>
2016-09-12 22:55:17 -0400

    tweak nsdata conformance to help linux

--- Sources/GenomeFoundation/Foundation.swift
+++ Sources/GenomeFoundation/Foundation.swift
@@ -1,25 +1,6 @@
 import Foundation
 
-extension NSData: NodeRepresentable {
-    public func makeNode(context: Context = EmptyNode) throws -> Node {
-        var bytes = [UInt8](repeating: 0, count: length)
-        getBytes(&bytes, length: bytes.count)
-        let data = Data(bytes: bytes)
-        let js = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-        return Node(any: js)
-    }
-}
-
-#if Xcode
-extension NSData: NodeConvertible {}
-extension NodeInitializable where Self: NSData {
-    public init(node: Node, in context: Context) throws {
-        let any = node.any
-        let data = try JSONSerialization.data(withJSONObject: any, options: .init(rawValue: 0))
-        self.init(data: data)
-    }
-}
-#endif
+// MARK: Data
 
 extension Data: NodeRepresentable {
     public func makeNode(context: Context = EmptyNode) throws -> Node {
@@ -35,3 +16,31 @@ extension Data: NodeConvertible {
         self = data
     }
 }
+
+// MARK: NSData
+
+extension NSData: NodeRepresentable {
+    public func makeNode(context: Context = EmptyNode) throws -> Node {
+        var bytes = [UInt8](repeating: 0, count: length)
+        getBytes(&bytes, length: bytes.count)
+        let data = Data(bytes: bytes)
+        let js = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        return Node(any: js)
+    }
+}
+
+// MARK: Hacky Magic
+
+public protocol DataInitable {
+    init(data: Data)
+}
+extension NSData: DataInitable {}
+extension DataInitable {
+    public init(node: Node, in context: Context) throws {
+        let any = node.any
+        let data = try JSONSerialization.data(withJSONObject: any, options: .init(rawValue: 0))
+        self.init(data: data)
+    }
+}
+
+extension NSData: NodeConvertible {}
