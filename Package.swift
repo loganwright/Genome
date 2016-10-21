5a70d99f952f08a86e5c1429d44581468c2b1d80
Logan Wright <logan.william.wright@gmail.com>
2016-10-21 13:41:25 -0400

    updating node

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/node.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 1)
     ],
     exclude: [
         "Sources/Genome/CoreData"
