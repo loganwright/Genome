6fa074327dba7fe7b6dd5e825d91e2a1542681e8
Logan Wright <logan.william.wright@gmail.com>
2017-04-18 19:42:26 +0200

    remove nsdata conveniences

--- Sources/GenomeFoundation/Foundation.swift
+++ Sources/GenomeFoundation/Foundation.swift
@@ -16,30 +16,3 @@ extension Data: NodeConvertible {
         self = data
     }
 }
-
-// MARK: NSData
-
-extension NSData: NodeRepresentable {
-    public func makeNode(context: Context = EmptyNode) throws -> Node {
-        var bytes = [UInt8](repeating: 0, count: length)
-        getBytes(&bytes, length: bytes.count)
-        let data = Data(bytes: bytes)
-        let js = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-        return Node(any: js)
-    }
-}
-
-
-#if Xcode
-/*
-Linux won't compile this.
-*/
-extension NSData: NodeConvertible {}
-extension NodeConvertible where Self: NSData {
-    public init(node: Node, in context: Context) throws {
-        let any = node.any
-        let data = try JSONSerialization.data(withJSONObject: any, options: .init(rawValue: 0))
-        self.init(data: data)
-    }
-}
-#endif
